69f6d58c14da7c63d91d5671aae13f89cae17b54
NguyenAndrew <anguyen29@wisc.edu>
2020-07-28 22:43:07 -0500

    Update example 4 description + create example 4

--- README.md
+++ README.md
@@ -3,8 +3,11 @@ Example of using Jest and Typescript in Node
 
 ## Examples (Located in src folder)
 
-* sum.test.ts - Hello World! Test
+Note: The numbers don't indicate, if one example is better than another. Examples are provided to be unblockers and help guide you when writing your own tests.
+
+* sum.test.ts - Hello World! Single Test
 * aws-sdk-example-1.test.ts - Hello World! - More Advanced Test. Single Test, single mock.
 * aws-sdk-example-2.test.ts - Multiple Tests, using Jest doMocks.
 * aws-sdk-example-3.test.ts - Multiple Tests, allows for default implementation, using type casting and mock implementation once.
-* aws-sdk-example-4.test.ts - **IN PROGRESS** Uses spys to reset mocks, and to make sure mocks are called as intended.
+* aws-sdk-example-4.test.ts - Multiple Tests, using Jest Mock Module.
+* aws-sdk-example-5.test.ts - **IN PROGRESS** Uses spys to reset mocks, and to make sure mocks are called as intended.
---
+++ src/aws-sdk-example-4.test.ts
@@ -0,0 +1,50 @@
+// References
+// https://jestjs.io/docs/en/mock-functions#mocking-modules - Mocking modules from official Jest documentation
+
+
+import sqsSend from "./aws-sdk-example";
+
+// Mocks
+import aws, {SQS} from "aws-sdk";
+
+jest.mock('aws-sdk');
+// Note: Look for a better alternative for mockAws
+// Any cast here makes it harder to discover available methods on aws. For now, try directly calling aws to discover methods.
+const mockAWS = aws as jest.Mocked<any>
+
+
+test('send success', async () => {
+    const sendMessageMock = jest.fn().mockReturnValue({
+        promise: jest.fn()
+    });
+
+    const SQSMocked = {
+        sendMessage: sendMessageMock,
+    };
+
+    mockAWS.SQS = jest.fn(() => SQSMocked)
+
+    let output: String = await sqsSend();
+    expect(output).toBe("Success!");
+});
+
+test('throw failure', async () => {
+
+    const sendMessageMock = jest.fn().mockReturnValue({
+        promise: jest.fn().mockRejectedValue(new Error('Error sending message'))
+    });
+
+    const SQSMocked = {
+        sendMessage: sendMessageMock,
+    };
+
+    mockAWS.SQS = jest.fn(() => SQSMocked)
+
+    try {
+        await sqsSend();
+        fail('Test should have thrown exception');
+    } catch (err) {
+        // Notice: Using toEqual instead of the toBe.
+        expect(err).toEqual(new Error('Error sending message'));
+    }
+});
