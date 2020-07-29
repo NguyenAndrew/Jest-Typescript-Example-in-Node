f0b8d0e3b3b3713aecb8cdad9c0660ca2345c5e6
NguyenAndrew <anguyen29@wisc.edu>
2020-07-28 22:44:12 -0500

    Cleanup example 4, mocking modules

--- src/aws-sdk-example-4.test.ts
+++ src/aws-sdk-example-4.test.ts
@@ -1,17 +1,15 @@
 // References
 // https://jestjs.io/docs/en/mock-functions#mocking-modules - Mocking modules from official Jest documentation
 
-
 import sqsSend from "./aws-sdk-example";
 
 // Mocks
-import aws, {SQS} from "aws-sdk";
+import aws from "aws-sdk";
 
 jest.mock('aws-sdk');
 // Note: Look for a better alternative for mockAws
 // Any cast here makes it harder to discover available methods on aws. For now, try directly calling aws to discover methods.
 const mockAWS = aws as jest.Mocked<any>
-
 
 test('send success', async () => {
     const sendMessageMock = jest.fn().mockReturnValue({
